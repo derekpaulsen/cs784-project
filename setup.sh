fec40b4cb3194ccfcd6c10368dff057f22545c95
derekpaulsen <derekpaulsensi@gmail.com>
2019-03-07 11:59:15 -0600

    asjdhasd

--- setup.sh
+++ setup.sh
@@ -2,10 +2,15 @@
 sudo apt update -y
 sudo apt upgrade -y
 
-
+#install java
+sudo apt install software-properties-common -y
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt update
 sudo apt install java-common -y
+sudo apt install oracle-java8-installer
+
 sudo apt install scala -y
-sudo apt-get install openssh-server openssh-client -y 
+sudo apt install openssh-server openssh-client -y 
 #utilities
 sudo apt install vim tmux htop ipython3 python3-pip -y
 
