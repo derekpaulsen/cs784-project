2ffa6d161ddd80f50fc648cfad754cce4eea4aaa
derekpaulsen <derekpaulsensi@gmail.com>
2019-03-07 10:20:31 -0600

    setup...

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ sudo apt install java-common -y
 sudo apt install scala -y
 sudo apt-get install openssh-server openssh-client -y 
 #utilities
-sudo apt install nvim tmux htop ipython3 python3-pip -y
+sudo apt install vim tmux htop ipython3 python3-pip -y
 
 pip3 install pandas numpy scipy
 
